e6bcb473c1ba3fd8cd5382aa8b385d454a48b6ad
theydontknowmeson4497 <mm4497@srmist.edu.in>
2026-02-27 14:51:53 +0530

    UC3: Reversing the String Method

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,39 +2,48 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class PalindromeCheckerApp{
-    public static void main(String[] args){
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version 1.0");
         System.out.println("System Initialized Successfully");
 
 
-            Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Enter a string: ");
-            String text = scanner.nextLine();
+        System.out.print("Enter a string: ");
+        String text = scanner.nextLine();
 
-            Deque<Character> deque = new LinkedList<>();
+        Deque<Character> deque = new LinkedList<>();
 
-            for (int i = 0; i < text.length(); i++) {
-                deque.addLast(text.charAt(i));
+        for (int i = 0; i < text.length(); i++) {
+            deque.addLast(text.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                isPalindrome = false;
+                break;
             }
+        }
 
-            boolean isPalindrome = true;
-
-            while (deque.size() > 1) {
-                if (deque.removeFirst() != deque.removeLast()) {
-                    isPalindrome = false;
-                    break;
-                }
-            }
-
-            if (isPalindrome) {
-                System.out.println("The string \"" + text + "\" is a palindrome.");
-            } else {
-                System.out.println("The string \"" + text + "\" is NOT a palindrome.");
-            }
-
+        if (isPalindrome) {
+            System.out.println("The string \"" + text + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + text + "\" is NOT a palindrome.");
+        }
+        int strLength = text.length();
+        String reverse = "";
+        for (int i = (strLength - 1); i >= 0; --i) {
+            reverse = reverse + text.charAt(i);
+        }
+        if (text.toLowerCase().equals(reverse.toLowerCase())) {
+            System.out.println(text + " is a Palindrome String.");
+        } else {
+            System.out.println(text + " is not a Palindrome String.");
             scanner.close();
         }
     }
+}
